c0875a31e4d741276f11c4005af3dec8101045b5
Luciolima1990 <luciolima.oliveira@gmail.com>
2026-06-12 13:15:14 -0300

    feat: CRUD completo S02 - POST, PUT, DELETE

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
+let nextId = 1;
 
 app.use(express.json());
 
-const tasks = [
-  {id: 1, title:'teste 1', status: 'ativo'},
-  {id: 2, title: 'teste 2', status: 'ativo'},
-  {id: 3, title: 'teste 3', status: 'desativado'}
+let tasks = [
+  {id: 1, title:'teste 1', description: 'descrição 1', status: 'ativo', priority: 'Alta'},
+  {id: 2, title: 'teste 2', description: 'descrição 2', status: 'inativo', priority: 'Media'},
 ]
 
 app.get('/', (req, res) => {
@@ -19,8 +19,62 @@ app.get('/tasks', (req, res) => {
 
 app.get('/tasks/:id', (req,res) => {
  const task = tasks.find(busca => busca.id === Number(req.params.id));
+ if(!task){
+  return res.status(404).json({error: 'Não encontrado'})
+ }
 res.json(task);
 })
+
+app.post('/tasks',(req,res) =>{
+
+    const {title, description, status, priority} = req.body;
+
+    const newTask = {
+      id: nextId++,
+      title: title,
+      description: description,
+      status: status,
+      priority: priority,
+    }
+    tasks.push(newTask);
+
+    res.status(201).json(newTask);
+
+})
+
+app.put('/tasks/:id', (req, res) =>{
+  const {id} = req.params;
+  const { title, description, status, priority } = req.body
+  const index = tasks.findIndex(t => t.id === Number(id));
+  if (index === -1){
+    return res.status(404).json({error: 'Não encontrado'})
+  }
+  tasks[index] = {
+    ...tasks[index],
+    title,
+    description,
+    status,
+    priority
+  }
+  res.json(tasks[index]);
+
+})
+
+app.delete('/tasks/:id', (req,res) =>{
+   const {id} = req.params;
+
+   const tasksExists = tasks.find(t => t.id === Number(id));
+   if (!tasksExists){
+    return res.status(404).json({error: 'Não encontrado'});
+   }
+    tasks = tasks.filter(t => t.id !== Number(id));
+
+   res.status(204).send();
+
+
+})
+
+
 app.listen(3000, () => {
   console.log('Servidor rodando em localhost:3000');
 });
